c2d73304414796b84aec79ce51fc196ca0705011
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2023-08-04 02:06:22 -0500

    notificacion

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      console.log("Push Notificacion recibida",remoteMessage);
+    }); 
+
+    const topic =messaging()
+    .subscribeToTopic('stevengualpa')
+    .then(()=> console.log('susvirofosfs'));
+
+    const back =messaging().setBackgroundMessageHandler(async (remoteMessage)=>{
+      console.log('en bacm',remoteMessage);
+    })
+    return ()=>{
+back();
+topic();
+unsubscribe();
+
+    } ;
+  }, []);
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
