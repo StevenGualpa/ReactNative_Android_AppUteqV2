77c760f7470f1a1ffc71381eab42c5cf0321de3a
Steven Gualpa <68717276+StevenGualpa@users.noreply.github.com>
2023-08-04 01:03:17 -0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
   return (
